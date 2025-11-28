82e906baea7bd7eb0c5d304cfc15bb61cfbdff40
kanterbraze <86393473+kanterbraze@users.noreply.github.com>
2025-11-28 22:35:42 +0800

    Save changes before pull

--- deploy.sh
+++ deploy.sh
@@ -20,11 +20,6 @@ git checkout -B main
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git main
+git push -f https://github.com/kanterbraze/singapore-stay-guide.git main:gh-pages
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git main:gh-pages
-
-echo "Build created and git repo initialized in /dist."
-echo "To deploy, you need to link this to your remote repository."
+echo "Deploying to https://kanterbraze.github.io/singapore-stay-guide/"
